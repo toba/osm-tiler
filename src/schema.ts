6a9ca39885a2ba2b3d42128379227af68f59b0d2
Jason Abbott <jasonabbott@Jasons-MacBook.local>
2020-01-14 16:06:13 -0700

    eip

--- src/schema.ts
+++ src/schema.ts
@@ -1,1 +1,23 @@
 // https://github.com/openmaptiles/openmaptiles
+// http://blog.functionalfun.net/2010/10/getting-started-with-protocol-buffers.html
+/**
+ * @see https://openmaptiles.org/schema/
+ */
+export const enum Schema {
+   AerodromeLabel = 'aerodrome_label',
+   Aeroway = 'aeroway',
+   Boundary = 'boundary',
+   Building = 'building',
+   HouseNumber = 'housenumber',
+   LandCover = 'landcover',
+   LandUse = 'landuse',
+   MountainPeak = 'mountain_peak',
+   Park = 'park',
+   Place = 'place',
+   POI = 'poi',
+   Transportation = 'transportation',
+   TransportationLabel = 'transportation_name',
+   Water = 'water',
+   WaterName = 'water_name',
+   WaterWay = 'waterway'
+}
